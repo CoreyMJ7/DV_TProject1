65fcbdd23a1ae84591f9cfb76d870390c1856cc5
RyanBilla <blackmist2008@gmail.com>
2015-10-27 17:57:12 -0500

    beginning of RMD

--- 03 R SQL Visualizations/R SQL.R
+++ 03 R SQL Visualizations/R SQL.R
@@ -62,92 +62,92 @@ ggplot() +
         geom_params=list(alpha=0.50), 
         position=position_identity()
   )
-
-
-
-#Graph 3: Fare vs Survived w/ As Character
-ggplot() + 
-  coord_cartesian() + 
-  scale_x_discrete() +
-  scale_y_continuous() +
-  #facet_wrap(~SURVIVED) +
-  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
-  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
-  labs(title='Titanic') +
-  labs(x="SURVIVED", y=paste("Fare")) +
-  layer(data=df2, 
-        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="point",
-        geom_params=list(), 
-        #position=position_identity()
-        position=position_jitter(width=0.3, height=0)
-  )
-
-
-
-# Graph 4: Fare vs Survived w/Class Separation
-ggplot() + 
-  coord_cartesian() + 
-  scale_x_discrete() +
-  scale_y_continuous() +
-  facet_grid(PCLASS~SURVIVED, labeller=label_both) +
-  labs(title='Titanic') +
-  labs(x="SURVIVED", y=paste("FARE")) +
-  layer(data=df2, 
-        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="point",
-        geom_params=list(), 
-        #position=position_identity()
-        position=position_jitter(width=0.3, height=0)
-  )
-
-
-
-
-
-#Graph 5: Fare vs Survived w/P class where age <= 10
-ggplot() + 
-  coord_cartesian() + 
-  scale_x_discrete() +
-  scale_y_continuous() +
-  #facet_wrap(~SURVIVED) +
-  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
-  facet_grid(PCLASS~SURVIVED, labeller=label_both) +
-  labs(title='Titanic where age <= 10') +
-  labs(x="SURVIVED", y=paste("Fare")) +
-  layer(data=df3, 
-        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="point",
-        geom_params=list(), 
-        #position=position_identity()
-        position=position_jitter(width=0.3, height=0)
-  )
-
-
-
-#Graph 6: Age vs Sex w/Embark and Survived - UNIQUE GROUP VISUALIZATION
-#Graphs the age vs sex of the passengers and separates this data by where the passenger embarked from and whether they survived
-ggplot() + 
-  coord_cartesian() + 
-  scale_x_discrete() +
-  scale_y_continuous() +
-  #facet_wrap(~SURVIVED) +
-  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
-  facet_grid(SURVIVED~EMBARKED, labeller=label_both) +
-  labs(title='Titanic Age >= 40 vs Sex By Embarked Location & Survival') +
-  labs(x="SEX", y=paste("AGE")) +
-  layer(data=df4, 
-        mapping=aes(x=SEX, y=as.numeric(as.character(AGE)), color=SEX), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="point",
-        geom_params=list(), 
-        #position=position_identity()
-        position=position_jitter(width=0.4, height=0)
-  )
+# 
+# 
+# 
+# #Graph 3: Fare vs Survived w/ As Character
+# ggplot() + 
+#   coord_cartesian() + 
+#   scale_x_discrete() +
+#   scale_y_continuous() +
+#   #facet_wrap(~SURVIVED) +
+#   #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
+#   #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+#   labs(title='Titanic') +
+#   labs(x="SURVIVED", y=paste("Fare")) +
+#   layer(data=df2, 
+#         mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
+#         stat="identity", 
+#         stat_params=list(), 
+#         geom="point",
+#         geom_params=list(), 
+#         #position=position_identity()
+#         position=position_jitter(width=0.3, height=0)
+#   )
+# 
+# 
+# 
+# # Graph 4: Fare vs Survived w/Class Separation
+# ggplot() + 
+#   coord_cartesian() + 
+#   scale_x_discrete() +
+#   scale_y_continuous() +
+#   facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+#   labs(title='Titanic') +
+#   labs(x="SURVIVED", y=paste("FARE")) +
+#   layer(data=df2, 
+#         mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
+#         stat="identity", 
+#         stat_params=list(), 
+#         geom="point",
+#         geom_params=list(), 
+#         #position=position_identity()
+#         position=position_jitter(width=0.3, height=0)
+#   )
+# 
+# 
+# 
+# 
+# 
+# #Graph 5: Fare vs Survived w/P class where age <= 10
+# ggplot() + 
+#   coord_cartesian() + 
+#   scale_x_discrete() +
+#   scale_y_continuous() +
+#   #facet_wrap(~SURVIVED) +
+#   #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
+#   facet_grid(PCLASS~SURVIVED, labeller=label_both) +
+#   labs(title='Titanic where age <= 10') +
+#   labs(x="SURVIVED", y=paste("Fare")) +
+#   layer(data=df3, 
+#         mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX), 
+#         stat="identity", 
+#         stat_params=list(), 
+#         geom="point",
+#         geom_params=list(), 
+#         #position=position_identity()
+#         position=position_jitter(width=0.3, height=0)
+#   )
+# 
+# 
+# 
+# #Graph 6: Age vs Sex w/Embark and Survived - UNIQUE GROUP VISUALIZATION
+# #Graphs the age vs sex of the passengers and separates this data by where the passenger embarked from and whether they survived
+# ggplot() + 
+#   coord_cartesian() + 
+#   scale_x_discrete() +
+#   scale_y_continuous() +
+#   #facet_wrap(~SURVIVED) +
+#   #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
+#   facet_grid(SURVIVED~EMBARKED, labeller=label_both) +
+#   labs(title='Titanic Age >= 40 vs Sex By Embarked Location & Survival') +
+#   labs(x="SEX", y=paste("AGE")) +
+#   layer(data=df4, 
+#         mapping=aes(x=SEX, y=as.numeric(as.character(AGE)), color=SEX), 
+#         stat="identity", 
+#         stat_params=list(), 
+#         geom="point",
+#         geom_params=list(), 
+#         #position=position_identity()
+#         position=position_jitter(width=0.4, height=0)
+#   )
