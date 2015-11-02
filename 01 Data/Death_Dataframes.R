4f1b42e103783e031e292d04273fc3d3ec4408f8
RyanBilla <blackmist2008@gmail.com>
2015-11-02 14:11:59 -0600

    finishing up

--- 01 Data/Death_Dataframes.R
+++ 01 Data/Death_Dataframes.R
@@ -3,7 +3,7 @@ require("RCurl")
 require("ggplot2")
 
 # Dataframe created from CSV table created in SQL
-death_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from DEATH where NUMBER_OF_DEATHS < 100000"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_nar784', PASS='orcl_nar784', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+death_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from DEATH where NUMBER_OF_DEATHS < 100000"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='C##cs329e_nar784', PASS='orcl_nar784', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
 summary(death_df)
 head(death_df)
